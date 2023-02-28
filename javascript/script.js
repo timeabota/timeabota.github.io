d822005287edbeda6f82bb4a19b679abbe3a88e0
timeabota <timea.bota@gmail.com>
2023-02-28 07:21:02 +0100

    dotted background with js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,23 @@
 const text = document.querySelector('.cir-text p');
-text.innerHTML = text.innerText.split("").map((char, i) => `<span style="transform:rotate(${i * 9}deg)">${char}</span>`
+text.innerHTML = text.innerText.split("").map((char, i) => `<span style="transform:rotate(${i * 18}deg)">${char}</span>`
 ).join("")
+
+
+function addElement() {
+   
+   const dottedContainer = document.querySelector('.dotted-background'); 
+
+   const boxDiv = document.createElement('div');
+   boxDiv.classList.add('row');
+
+   dottedContainer.appendChild(boxDiv);
+
+    for (let i = 0; i < 8; i++) {
+        const boxSpan = document.createElement('span');
+        boxDiv.appendChild(boxSpan);
+    }
+}
+
+for (let i = 0; i < 25; i++) {
+    addElement();
+}
