9556be638780fc55b049b717d40c014c2dc7d5d3
timeabota <timea.bota@gmail.com>
2023-03-09 14:57:51 +0100

    splash screen v1

--- javascript/script.js
+++ javascript/script.js
@@ -136,3 +136,35 @@ if (window.innerWidth <= 768) {
 const removeClasses = (mirol, mit) => {
     document.querySelector(`${mirol}`).classList.remove(`${mit}`);    
 }
+
+
+//splash screen
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo-intro');
+
+window.addEventListener('DOMContentLoaded', ()=> {
+
+    setTimeout(()=>{
+        
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active-intro');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active-intro');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            })
+        }, 3000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 3300);
+    })
+})
