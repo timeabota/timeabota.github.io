6cdedbe16fa10937ccc964f53d66520879468d2e
timeabota <timea.bota@gmail.com>
2023-03-17 19:28:16 +0100

    back to my work respo

--- javascript/portfolio_script.js
+++ javascript/portfolio_script.js
@@ -1,38 +1,38 @@
 //splash screen
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo-intro');
+// let intro = document.querySelector('.intro');
+// let logo = document.querySelector('.logo-header');
+// let logoSpan = document.querySelectorAll('.logo-intro');
 
-window.addEventListener('DOMContentLoaded', ()=> {
+// window.addEventListener('DOMContentLoaded', ()=> {
 
-    setTimeout(()=>{
+//     setTimeout(()=>{
         
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active-intro');
-            }, (idx + 1) * 400)
-        });
+//         logoSpan.forEach((span, idx)=>{
+//             setTimeout(()=>{
+//                 span.classList.add('active-intro');
+//             }, (idx + 1) * 400)
+//         });
 
-        setTimeout(()=>{
-            logoSpan.forEach((span, idx)=>{
+//         setTimeout(()=>{
+//             logoSpan.forEach((span, idx)=>{
 
-                setTimeout(()=>{
-                    span.classList.remove('active-intro');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })
-        }, 1000);
+//                 setTimeout(()=>{
+//                     span.classList.remove('active-intro');
+//                     span.classList.add('fade');
+//                 }, (idx + 1) * 50)
+//             })
+//         }, 1000);
 
-        setTimeout(()=>{
-            intro.style.top = '-100vh';
-        }, 1300);
-    })
-})
+//         setTimeout(()=>{
+//             intro.style.top = '-100vh';
+//         }, 1300);
+//     })
+// })
 
-const topContainer = document.querySelector('.top-container');
-const mobilePic = document.querySelectorAll('.pic-mobile-container div');
-const desktopPic = document.querySelectorAll('.pic-desktop-container div');
-const bottomContainer = document.querySelector('.bottom-container');
+// const topContainer = document.querySelector('.top-container');
+// const mobilePic = document.querySelectorAll('.pic-mobile-container div');
+// const desktopPic = document.querySelectorAll('.pic-desktop-container div');
+// const bottomContainer = document.querySelector('.bottom-container');
 
 //observer
 // const observer = new IntersectionObserver(entries => {
